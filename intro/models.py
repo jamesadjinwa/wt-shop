4c1081cb0be7f145ae8d0e3bd9f03f2e3b4f523a
jamesadjinwa <jaymee126@gmail.com>
2016-11-08 08:14:27 +0100

    Contact form

--- intro/models.py
+++ intro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    phone_regex = forms.RegexField(regex=r'^\+237\d{9}$',
+                                   message="Phone number must be entered in the format: '+237'. "
+                                           "Up to 15 digits allowed."
+                                   )
+    contact_phone = forms.CharField(required=True, validators=[phone_regex])
+    contact_message = forms.CharField(required=True, widget=forms.Textarea)
--- intro/views.py
+++ intro/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def intro(request):
+
     return render(request, 'intro/index.html', {})
+
+
+def contact(request):
+
+    form_class = ContactForm
+
+    return render(request, 'intro/index.html/#contact', {
+        'form': form_class,
+    })
